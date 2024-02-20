e15c19870b9cdd25eae6b78bd76b0f750e6eeeb2
izuca <adriel200010@hotmail.com>
2024-02-19 22:47:24 -0300

    Read e Create do Crud

---
+++ src/controllers/propertiesController.js
@@ -0,0 +1,35 @@
+const {log} = require("mercedlogger")
+const { PrismaClient } = require("@prisma/client")
+const { Router } = require("express")
+
+const prisma = new PrismaClient()
+
+//Read All
+const getProperties = async()=>{
+	const allImoveis = await prisma.properties.findMany();
+	console.log(allImoveis)
+}
+
+//Create
+const createProperty = async()=>{
+	const property = await prisma.properties.create({
+		data: {
+			title: "Casa aconchegante perto do parque",
+			description: "Uma casa encantadora com jardim",
+			address: "Rua das Flores,123",
+			propertyType:"Casa",
+			rentAmount: 2000.00,
+			available: true,
+			city: "Anápolis",
+			state: "Goiás"
+		}
+	})
+
+	console.log("propriedade criada")
+}
+
+
+module.exports = {
+	getProperties,
+	createProperty
+}
