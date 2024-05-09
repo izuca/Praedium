23a91bc3017cf230fe05c244a634d32d59914d17
izuca <adriel200010@hotmail.com>
2024-05-09 19:54:24 -0300

    Responsividade Pagina do Imovel

--- frontend/src/app/imoveis/[imovelId]/page.tsx
+++ frontend/src/app/imoveis/[imovelId]/page.tsx
@@ -9,8 +9,8 @@ export default function ImovelId({ params }: { params: { imovelId: string } }) {
         <Image
           src="/hero_background.jpg"
           alt="teste"
-          width={600}
-          height={600}
+          width={800}
+          height={800}
         />
         <div className="grid-cols-2 hidden lg:grid">
           <Image
@@ -18,53 +18,43 @@ export default function ImovelId({ params }: { params: { imovelId: string } }) {
             alt="teste"
             width={200}
             height={200}
-            className="w-fit"
+            className="w-fit h-full"
           />
           <Image
             src="/hero_background.jpg"
             alt="teste"
             width={200}
             height={200}
-            className="w-fit"
+            className="w-fit h-full"
           />
           <Image
             src="/hero_background.jpg"
             alt="teste"
             width={200}
             height={200}
-            className="w-fit"
+            className="w-fit h-full"
           />
           <Image
             src="/hero_background.jpg"
             alt="teste"
             width={200}
             height={200}
-            className="w-fit"
+            className="w-fit h-full"
           />
         </div>
       </div>
-      <div className="hero flex justify-center text-black gap-40">
+      <div className="hero flex flex-col lg:flex-row justify-center text-black gap-14 lg:gap-40">
         <div className="flex flex-col gap-9">
           <div className="flex flex-col text-center px-12 lg:px-0 lg:text-left">
             <h3 className="font-serif text-4xl ">Título da Obra</h3>
             <p className="text-lg font-light ">Descrição</p>
           </div>
           <div className="flex flex-col gap-2">
-            <h3 className="font-serif text-4xl ">Localização</h3>
-            <div className="flex flex-row items-center text-center px-12 lg:px-0 lg:text-left">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="50px"
-                height="50px"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill="#2c2c2c"
-                  d="m19.485 20.154l-6.262-6.262q-.75.639-1.725.989t-1.96.35q-2.402 0-4.066-1.663T3.808 9.503T5.47 5.436t4.064-1.667t4.068 1.664T15.268 9.5q0 1.042-.369 2.017t-.97 1.668l6.262 6.261zM9.539 14.23q1.99 0 3.36-1.37t1.37-3.361t-1.37-3.36t-3.36-1.37t-3.361 1.37t-1.37 3.36t1.37 3.36t3.36 1.37"
-                />
-              </svg>
+            <h3 className="font-serif text-4xl text-center lg:text-left">Localização</h3>
+            <div className="flex flex-row items-center text-center px-12 lg:px-0 lg:text-left gap-5">
+              <Image src="/local.svg" alt="Local" width={100} height={100}/>
               <div className="flex flex-col">
-                <p className="text-lg font-light ">Bairro</p>
+                <p className="text-2xl font-light ">Bairro</p>
                 <p className="text-sm font-light ">Cidade, CEP</p>
               </div>
             </div>
