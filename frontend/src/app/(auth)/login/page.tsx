32a91270260c9933855646042e6c9fae2c61ddbe
izuca <adriel200010@hotmail.com>
2024-05-15 20:17:09 -0300

    Hook de autenticação

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,11 +1,11 @@
-import { SignupForm } from "./signupForm"
+
 
 
 
 export default function Login(){
     return(
         <div className="bg-white p-8 w-96">
-        {/* <h1 className="text-2xl font-serif mb-4">Faça login</h1>
+        <h1 className="text-2xl font-serif mb-4">Faça login</h1>
         <form>
             <div className="mb-4">
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">E-mail</label>
@@ -13,12 +13,12 @@ export default function Login(){
             </div>
             <div className="mb-4">
                 <label htmlFor="password" className="block text-sm font-medium text-gray-700">Senha</label>
-                <input type="password" id="password" name="password" className="mt-1 p-2 w-full border rounded"/>
+                <input type="password" id="password" name="password" className="mt-1 p-2 w-full border"/>
             </div>
             <button type="submit" className="bg-orange text-white-900 px-4 py-2 hover:bg-black">Entrar</button>
-        </form> */}
+        </form>
 
-        <SignupForm/>
+        
     </div>
     )
 }
